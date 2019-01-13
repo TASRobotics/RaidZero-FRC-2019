5a765a4b54a3e5352bb7ad93a24f696606db7181
Kof223 <fidoko204@gmail.com>
2019-01-13 11:34:36 +0800

    Fixed angle issues and added more comments.
    The units are arbitrary, so now they are inches.

--- MATLAB/RobotPath.m
+++ MATLAB/RobotPath.m
@@ -1,45 +1,43 @@
 function motionProfile = RobotPath()
-% r == returns
-
-    CruiseVel = 10; % Units: ??
-    TargetAcc = 20; % Units: ??
+    % Units are in inches
+    CruiseVel = 10; % Units: Inches per 100 ms
+    TargetAcc = 20; % Units: Inches per 100 ms per ms 
     Updatetime = 0.1; % Default update time: 0.1 DON'T CHANGE!
-    DeltaD = Updatetime*CruiseVel;
-    Nacc = ceil(CruiseVel^2/(TargetAcc*DeltaD)); % ??
-    % ceil - rounds to nearest integer (math.ceiling)
-    Waypoints = [0 0; 3 6; 8 8];
-    numPoints = length(Waypoints(:,1));
-    % r 7
-    interpoints = 0:.01:1;
-    % r 0 -> 1 incrementing 0.01
+    DeltaD = Updatetime*CruiseVel; % Self Explanatory
     
-    Direction = Waypoints(1:numPoints-1,:)-Waypoints(2:numPoints,:);
-    Dist = sqrt(diag(Direction*Direction'));
+    Waypoints = [0 0; 1 12; 36 72; 83 95; 96 96]; % Input the waypoints here! Units: Inches
+    
+    numPoints = length(Waypoints(:,1)); % Number of waypoints
+    interpoints = 0:.01:1; % Create points from 0 -> 1 incrementing 0.01
+    
+    Direction = Waypoints(1:numPoints-1,:) - Waypoints(2:numPoints,:);
+    Dist = sqrt(diag(Direction * Direction'));
     % finds distance between each point
     param = [0; cumsum(Dist)/sum(Dist)];
     % divides distance traveled by total distance
     
-    Fit = spline(param,Waypoints',interpoints);
+    Fit = spline(param, Waypoints', interpoints);
     % finds points in an arc based on points given by waypoint seperated by
     % values from param and limited (min and max value) by interpoints
     % scatter(Fit(1,:), Fit(2,:)); 
-    hold on;
     plot(Fit(1,:),Fit(2,:));
+    hold on;
     angles = makeAngles(Fit');
     % finds the angle in between each point from one point to next
     travelled = zeros(length(param),1);
     % initialize distance travelled matrix
     
     for i = 2:length(Fit)
-        travelled(i) = arclength(Fit(1,1:i),Fit(2,1:i));
+        travelled(i) = arclength(Fit(1,1:i), Fit(2,1:i));
     end
     % travelled gives you total distance travelled after going pass each
     % point of arclength
-    TotalDist=travelled(length(travelled))
+    TotalDist = travelled(length(travelled))
     % total distance travelled
     
-    Nvel = ceil(TotalDist/DeltaD - Nacc);
-    % ??
+    Nacc = ceil(CruiseVel^2 / (TargetAcc * DeltaD)); % ??
+    Nvel = ceil(TotalDist/DeltaD - Nacc); % ??
+    
     distmarks = zeros(2*Nacc+Nvel,1);
     velmarks = zeros(length(distmarks),1);
     for i=1:Nacc
@@ -54,11 +52,10 @@ function motionProfile = RobotPath()
     end
     times = 0:Updatetime:Updatetime*(length(distmarks)-1);
     dist2Param = spline(travelled,interpoints',distmarks);
-    anglemarks = spline(interpoints,angles,dist2Param);
+    anglemarks = spline(interpoints,angles,dist2Param)-90;
     quiver(spline(interpoints,Fit(1,:),dist2Param),spline(interpoints,Fit(2,:),dist2Param),cos(pi/180*spline(interpoints,angles,dist2Param)),sin(pi/180*spline(interpoints,angles,dist2Param)));
     motionProfile = [distmarks velmarks times' anglemarks];
     hold off;
-    
     
     function angles = makeAngles(trajectory)
         numpoints = length(trajectory(:,1));
@@ -66,6 +63,7 @@ function motionProfile = RobotPath()
         comparison = [trajectory(1,1) trajectory(1,2);
             trajectory;
             trajectory(numpoints,1) trajectory(numpoints,1)];
+        comparison(1,:) = []; % This is here to fix an extra row which messes up the initial angles. It removes the first row.
         angles = unwrap(atan2(comparison(2:numpoints+1,2)-comparison(1:numpoints,2),(comparison(2:numpoints+1,1)-comparison(1:numpoints,1))));
         angles = 360/(2*pi)*angles;
     end
