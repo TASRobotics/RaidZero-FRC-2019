8625d51344c5cdb2c116729198f261fd2cfb4c59
(∩ ͡° ͜ʖ ͡°)⊃━☆ﾟ. * ･ ｡ﾟ <usymmij@gmail.com>
2019-01-31 16:21:26 +0800

    Update Components.java

--- src/main/java/raidzero/robot/components/Components.java
+++ src/main/java/raidzero/robot/components/Components.java
@@ -22,7 +22,7 @@ public class Components {
      */
     public static void initialize() {
         base = new Base(0, 2, 4, 1, 3, 5, 0, 1, 0);
-        lift = new Lift(10, 11);
+        lift = new Lift(0, 1);
         arm = new Arm(12, 13);
         intake = new Intake(14, 2, 3);
     }
