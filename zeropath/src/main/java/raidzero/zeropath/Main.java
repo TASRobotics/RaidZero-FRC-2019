46bfb4ae1167a3e679cb7aa52e1d5218b856d4ec
qsctr <qsctr@users.noreply.github.com>
2019-02-19 12:48:03 +0800

    zeropath: Add custom gson deserializer for Point

--- zeropath/src/main/java/raidzero/zeropath/Main.java
+++ zeropath/src/main/java/raidzero/zeropath/Main.java
@@ -3,15 +3,28 @@ package raidzero.zeropath;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
 
 import raidzero.pathgen.PathGenerator;
+import raidzero.pathgen.Point;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         var reader = new BufferedReader(new InputStreamReader(System.in));
-        var gson = new Gson();
+        var gson = new GsonBuilder()
+            .registerTypeAdapter(Point.class, (JsonDeserializer<Point>) (json, type, context) -> {
+                    var object = json.getAsJsonObject();
+                    var x = object.get("x").getAsDouble();
+                    var y = object.get("y").getAsDouble();
+                    var angle = object.get("angle");
+                    if (angle == null) {
+                        return new Point(x, y);
+                    }
+                    return new Point(x, y, angle.getAsDouble());
+                })
+            .create();
         while (true) {
             var req = gson.fromJson(reader.readLine(), Request.class);
             var queryData = PathGenerator.getQueryData(req.waypoints);
@@ -20,7 +33,7 @@ public class Main {
             var yQueries = PathGenerator.query(t -> splines.y.value(t)[0], queryData);
             var res = new Response();
             res.path = new ZeroPathPoint[queryData.queryCount];
-            for (var i = 0; i < res.path.length; i++) {
+            for (var i = 0; i < queryData.queryCount; i++) {
                 res.path[i] = new ZeroPathPoint();
                 res.path[i].x = xQueries[i];
                 res.path[i].y = yQueries[i];
