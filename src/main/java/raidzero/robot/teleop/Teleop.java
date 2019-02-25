44530cd3fead02cee09ae60f32d25f1f7ad38d42
LouisAsanaka <25756888+LouisAsanaka@users.noreply.github.com>
2019-02-25 10:29:52 +0800

    Restored old teleop code for lift

--- src/main/java/raidzero/robot/teleop/Teleop.java
+++ src/main/java/raidzero/robot/teleop/Teleop.java
@@ -69,11 +69,15 @@ public class Teleop {
         double leftTriggerAxis1 = controller1.getTriggerAxis(kLeft);
 
         if (rightTriggerAxis1 > 0.1) {
-            liftPos += rightTriggerAxis1;
+            //liftPos += rightTriggerAxis1;
+            Components.getLift().movePercent(rightTriggerAxis1);
         } else if (leftTriggerAxis1 > 0.1) {
-            liftPos -= leftTriggerAxis1;
+            //liftPos -= leftTriggerAxis1;
+            Components.getLift().movePercent(-leftTriggerAxis1 * 0.5);
+        } else {
+            Components.getLift().movePercent(0);
         }
-        Components.getLift().movePosition(liftPos);
+        //Components.getLift().movePosition(liftPos);
 
         // Player 2
 
