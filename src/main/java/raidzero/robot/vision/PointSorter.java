c5fe8480846108e1db3ea56bb8452deb9ff239c6
Karakorum91 <shinyi@gmail.com>
2019-03-10 18:11:03 +0800

    PointSorter Documentation
    See Title

--- src/main/java/raidzero/robot/vision/PointSorter.java
+++ src/main/java/raidzero/robot/vision/PointSorter.java
@@ -7,25 +7,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Comparator;
 
+/**
+ * Helper class to sort points of tape targets.
+ */
 public class PointSorter {
 
     private double[] xcorns, ycorns;
 
+    /**
+     * Initialize object with the limelight-generated x and y corner arrays.
+     *
+     * @param xcorns double array of corner x-values
+     * @param ycorns double array of corner y-values
+     */
     public PointSorter(double[] xcorns, double[] ycorns) {
         this.xcorns = xcorns;
         this.ycorns = ycorns;
     }
 
+    /**
+     * Returns MatOfPoint2f with all corner points sorted.
+     */
     public MatOfPoint2f sortPoints() {
+        // Add points to a list and sort by x values
         List<Point> allPoints = new ArrayList<>();
         for(int j = 0; j < 8; j++) {
             allPoints.add(new Point(xcorns[j], ycorns[j]));
         }
         allPoints.sort(Comparator.comparing(point -> (point.x)));
 
+        // Separate list into those on the left tape and those on the right tape
         List<Point> leftPoints = allPoints.subList(0, 4);
         List<Point> rightPoints = allPoints.subList(4, 8);
 
+        // For each piece of tape, sort points vertically
         leftPoints.sort(Comparator.comparing(point -> (point.x + point.y * 1000)));
         rightPoints.sort(Comparator.comparing(point -> (point.x + point.y * 1000)));
 
